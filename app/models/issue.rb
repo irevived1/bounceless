9f9cc00d0555f895eade1f82ccce082dc4875e87
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-10-14 22:07:02 -0400

    built relationships

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,5 +1,5 @@
 class Issue < ApplicationRecord
   has_many :comments
-  belongs_to :user
-  belongs_to :department
+  belongs_to :student , foreign_key: :student_id, class_name: 'User'
+  belongs_to :department, foreign_key: :dept_id, class_name: 'Department'
 end
