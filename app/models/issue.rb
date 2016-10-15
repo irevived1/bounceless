fe3eee7e9564c21da7ff48e920a4051f2814e72d
aehmt <calisahmet@gmail.com>
2016-10-15 13:33:15 -0400

    elapsed time added to index page

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,5 +1,10 @@
 class Issue < ApplicationRecord
+  include ActionView::Helpers::DateHelper
   has_many :comments
   belongs_to :student , foreign_key: :student_id, class_name: 'User'
   belongs_to :department, foreign_key: :dept_id, class_name: 'Department'
+
+  def time_ago
+    "#{time_ago_in_words(self.created_at)} ago"
+  end
 end
