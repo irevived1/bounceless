1bcdc3f7d1168a713e56bbda765410c648f0213f
aehmt <calisahmet@gmail.com>
2016-10-14 20:47:27 -0400

    faculty controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'faculty/index'
+
+  get 'faculty/create'
+
+  get 'faculty/new'
+
+  get 'faculty/destroy'
+
+  get 'faculty/edit'
+
+  get 'faculty/update'
+
+  get 'faculty/show'
+
   get 'comment/create'
 
   get 'comment/destroy'
